5b5d0f634f31c3f6a761ea3a2fdf90af72d187d6
Alemayehu Getnet <alexgetnet34l@example.com>
2026-06-07 22:33:13 -0700

    it is expect for about

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main className="container">
+      <h1>About Us</h1>
+
+      <p>
+        This blogging application is built using
+        Next.js App Router.
+      </p>
+    </main>
+  );
+}
