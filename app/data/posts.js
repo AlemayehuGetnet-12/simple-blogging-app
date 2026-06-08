82e9344ecf2512db0f862e49a5772930f261e406
Alemayehu Getnet <alexgetnet34l@example.com>
2026-06-07 22:40:51 -0700

    add the inpute data usin js

---
+++ app/data/posts.js
@@ -0,0 +1,24 @@
+
+export const posts = [
+  {
+    id: 1,
+    title: "Getting Started with Next.js",
+    author: "Alemayehu Getet",
+    content:
+      "Next.js is a React framework for building fast web applications.",
+  },
+  {
+    id: 2,
+    title: "Understanding React Hooks",
+    author: "Alemayehu Getet",
+    content:
+      "Hooks allow you to use state and lifecycle features in functional components.",
+  },
+  {
+    id: 3,
+    title: "JavaScript ES6 Features",
+    author: "Alemayehu Getet",
+    content:
+      "ES6 introduced let, const, arrow functions, template literals and more.",
+  },
+];
