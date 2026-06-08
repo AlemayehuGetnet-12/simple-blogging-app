0b5e1d6bdb6a1215d145714020d444d014a48938
Alemayehu Getnet <alexgetnet34l@example.com>
2026-06-07 22:37:16 -0700

    add contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <main className="container">
+      <h1>Contact Us</h1>
+
+      <p>Email: alexgetet34@gmail.com</p>
+      <p>Phone: +251 931756792</p>
+      <p>Address: Addis Ababa, Ethiopia</p>
+      
+    </main>
+  );
+}
