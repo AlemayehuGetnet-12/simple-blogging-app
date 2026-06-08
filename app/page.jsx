10bf8117f38a94f9fc1a8db74bb389addeead425
Alemayehu Getnet <alexgetnet34l@example.com>
2026-06-07 22:42:36 -0700

    complet blog project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1>Welcome to Simple Blogging App</h1>
+
+      <p>
+        Read articles, explore blogs, and learn
+        Next.js.
+      </p>
+    </main>
+  );
+}
